249c69171455b36e0c24ce2b62c13eddbc72e2ed
Gijs Bartman <gijs.bartman@gmail.com>
2026-01-22 17:29:15 +0100

    chore: update name and version bump

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
-});
+  modules: ["@wxt-dev/module-react"],
+  manifest: {
+    name: "Portflow Extended",
+    description: "A production-grade extension to enhance Portflow features.",
+    version: "0.1.0",
+    permissions: ["storage", "tabs"],
+  },
+})
